808a37d2833b61e0a15ac1506dbcfae4906ea07e
SilviodeJesusMarinGomez <159966534+SilviodeJesusMarinGomez@users.noreply.github.com>
2024-04-17 00:22:29 +0000

    Version 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
+
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Space Invaders";
+    Element lienzo = hbox ({
+        spinner(21,3) | bold
+    })
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(lienzo)
+    );
+
+    Render(pantalla,lienzo);
+    pantalla.Print();
+    pantalla.ResetPosition();
+
+    return 0;
 }
